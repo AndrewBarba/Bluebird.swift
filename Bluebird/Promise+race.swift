f47bcb51e0096c10035b82ac1c4a8c93f88d5a24
Andrew Barba <abarba.77@gmail.com>
2016-10-01 17:23:57 -0400

    race

---
+++ Bluebird/Promise+race.swift
@@ -0,0 +1,20 @@
+//
+//  Promise+race.swift
+//  Bluebird
+//
+//  Created by Andrew Barba on 10/1/16.
+//  Copyright © 2016 Andrew Barba. All rights reserved.
+//
+
+extension Promise {
+
+    public static func race<A>(_ promises: Promise<A>...) -> Promise<A> {
+        return race(promises)
+    }
+
+    public static func race<A>(_ promises: [Promise<A>]) -> Promise<A> {
+        return Promise<A> { resolve, reject in
+            promises.forEach { $0.addHandlers(resolve, reject) }
+        }
+    }
+}
