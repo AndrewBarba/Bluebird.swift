766d2339b32a84348f37739af533d9346f2abbf0
Andrew Barba <abarba.77@gmail.com>
2016-10-01 18:30:41 -0400

    remove uikit

--- Bluebird/Bluebird.h
+++ Bluebird/Bluebird.h
@@ -6,14 +6,8 @@
 //  Copyright © 2016 Andrew Barba. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 //! Project version number for Bluebird.
 FOUNDATION_EXPORT double BluebirdVersionNumber;
 
 //! Project version string for Bluebird.
 FOUNDATION_EXPORT const unsigned char BluebirdVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Bluebird/PublicHeader.h>
-
-
