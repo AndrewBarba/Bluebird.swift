d3ebe29d710921cc8ad1eb577fb205f53dcc94b0
Andrew Barba <abarba.77@gmail.com>
2016-10-02 21:17:26 -0400

    Add spm support

---
+++ Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  Bluebird
+//
+//  Created by Andrew Barba on 10/2/16.
+//  Copyright © 2016 Andrew Barba. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Bluebird",
+    exclude: ["BluebirdTests"]
+)
