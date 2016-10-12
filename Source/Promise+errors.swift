cd0f32011f8afc2010b20cf6dbdf02f29e671239
Andrew Barba <abarba.77@gmail.com>
2016-10-11 23:07:03 -0400

    add cancelled error enum even though nothing calls it yet

--- Source/Promise+errors.swift
+++ Source/Promise+errors.swift
@@ -8,9 +8,11 @@
 
 /// Bluebird errors
 ///
+/// - cancelled:  thrown when a Promise was explicity cancelled
 /// - rangeError: thrown when a function that expects a non-empty array recieves an empty array
 /// - timeout:    thrown if a Promise does not resolve in the givin number of seconds
 public enum BluebirdError: Error {
+    case cancelled
     case rangeError
     case timeout(TimeInterval)
 }
